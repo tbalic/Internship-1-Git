e0fd2d09117b211145069bbc365c7219105b4107
tbalic <balic.tihana@gmail.com>
2020-11-19 18:56:09 +0100

    Added basic project architecture

---
+++ Internship1Git/Internship1Git/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Internship1Git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
